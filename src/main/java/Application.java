1a4b5bb0895528b6e88088f2ec61816bf2b8ac5c
Evgeniy Filipenko <berumoris@gmail.com>
2023-02-11 14:14:50 +0300

    Изменил реализацию задачи с удалнием отрицательных чисел из массива

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -9,13 +9,14 @@ public class Application {
         ArraysTasks arraysTasks = new ArraysTasks();
         int[] testArray = {-2,-1,0,2,3,4};
 
-//        functions.minutesToQuarterPrint(5);
-//        System.out.println(functions.calculate(10, 3, '%'));
-//
-//        double triangleAreaA = functions.getTriangleArea(20, 20);
-//        double triangleAreaB = functions.getTriangleArea(20, 20);
-//        functions.compareTriangle(triangleAreaA, triangleAreaB);
+        functions.minutesToQuarterPrint(5);
+        System.out.println(functions.calculate(10, 3, '%'));
+
+        double triangleAreaA = functions.getTriangleArea(20, 20);
+        double triangleAreaB = functions.getTriangleArea(20, 20);
+        functions.compareTriangle(triangleAreaA, triangleAreaB);
 
         System.out.println(Arrays.toString(arraysTasks.excludeNegativeNumbersWithLambda(testArray)));
+        System.out.println(Arrays.toString(arraysTasks.excludeNegativeNumbers(testArray)));
     }
 }
--- src/main/java/level1/computerscience/ArraysTasks.java
+++ src/main/java/level1/computerscience/ArraysTasks.java
@@ -4,6 +4,25 @@ import java.util.Arrays;
 
 public class ArraysTasks {
 
+    public int[] excludeNegativeNumbers(int[] originalArray){
+        int positiveNubersCounter = 0;
+        for (int j : originalArray) {
+            if (j >= 0) {
+                positiveNubersCounter++;
+            }
+        }
+
+        int[] positiveArray = new int[positiveNubersCounter];
+
+        for (int i = 0, j = 0; i < originalArray.length; i++) {
+            if (originalArray[i] >= 0){
+                positiveArray[j] = originalArray[i] * positiveNubersCounter;
+                j++;
+            }
+        }
+        return positiveArray;
+    }
+
         public int[] excludeNegativeNumbersWithLambda(int[] originalArray){
         int[] positiveArray = Arrays.stream(originalArray)
                                 .filter(element -> element >= 0)
