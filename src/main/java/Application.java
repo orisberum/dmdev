e6151cba0b4d80c33df444c9e7ce7719cc8b5d04
Evgeniy Filipenko <berumoris@gmail.com>
2023-02-11 16:34:31 +0300

    Добавил реализацию задачи с выводом элементов, которые больше среднего арифметического

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -8,6 +8,7 @@ public class Application {
         Functions functions = new Functions();
         ArraysTasks arraysTasks = new ArraysTasks();
         int[] testArray = {-2,-1,0,2,3,4};
+        char[] charArray = {'a', '6', 'y', 'P', 'T', 'q', '9', '+'};
 
         functions.minutesToQuarterPrint(5);
         System.out.println(functions.calculate(10, 3, '%'));
@@ -18,5 +19,7 @@ public class Application {
 
         System.out.println(Arrays.toString(arraysTasks.excludeNegativeNumbersWithLambda(testArray)));
         System.out.println(Arrays.toString(arraysTasks.excludeNegativeNumbers(testArray)));
+
+        arraysTasks.getMoreThenAverage(charArray);
     }
 }
--- src/main/java/level1/computerscience/ArraysTasks.java
+++ src/main/java/level1/computerscience/ArraysTasks.java
@@ -5,18 +5,19 @@ import java.util.Arrays;
 public class ArraysTasks {
 
     public int[] excludeNegativeNumbers(int[] originalArray){
-        int positiveNubersCounter = 0;
+        int positiveNumbersCounter = 0;
+
         for (int j : originalArray) {
             if (j >= 0) {
-                positiveNubersCounter++;
+                positiveNumbersCounter++;
             }
         }
 
-        int[] positiveArray = new int[positiveNubersCounter];
+        int[] positiveArray = new int[positiveNumbersCounter];
 
         for (int i = 0, j = 0; i < originalArray.length; i++) {
             if (originalArray[i] >= 0){
-                positiveArray[j] = originalArray[i] * positiveNubersCounter;
+                positiveArray[j] = originalArray[i] * positiveNumbersCounter;
                 j++;
             }
         }
@@ -31,5 +32,44 @@ public class ArraysTasks {
         return Arrays.stream(positiveArray)
                 .map(element -> element * positiveArray.length)
                 .toArray();
+    }
+
+    public int[] charArrayToIntArray(char[] charArray) {
+        int[] intArray = new int[charArray.length];
+
+        for (int i = 0; i < charArray.length; i++) {
+            intArray[i] = charArray[i];
+        }
+        return intArray;
+    }
+
+    public int average(int[] intArray){
+        int sum = 0;
+        for (int element : intArray){
+            sum += element;
+        }
+        return sum / intArray.length;
+    }
+
+    public void getMoreThenAverage(char[] charArray) {
+        int moreThenAverageCounter = 0;
+        int[] intArray = charArrayToIntArray(charArray);
+        int average = average(intArray);
+
+        for (int i = 0; i < charArray.length; i++) {
+            if (charArray[i] > average) {
+                moreThenAverageCounter++;
+            }
+        }
+
+        int[] moreThenAverageArray = new int[moreThenAverageCounter];
+
+        for (int i = 0, j = 0; i < intArray.length; i++){
+            if (intArray[i] > average) {
+                moreThenAverageArray[j] = intArray[i];
+                j++;
+            }
+        }
+        System.out.println(Arrays.toString(moreThenAverageArray));
     }
 }
